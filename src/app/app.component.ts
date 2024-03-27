dd19ac547986f5378467c54ce351b4ded3f710f3
JA8866 <109983530+JA8866@users.noreply.github.com>
2024-03-27 16:07:06 -0500

    Selecting a quiz and keeping track of it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -37,4 +37,10 @@ export class AppComponent implements OnInit {
   }
 
   quizzes: QuizDisplay[] =[];
+  selectedQuiz: QuizDisplay| undefined = undefined;
+
+  selectQuiz = (q: QuizDisplay) => {
+    this.selectedQuiz = q;
+    console.log(this.selectedQuiz);
+  };
 }
