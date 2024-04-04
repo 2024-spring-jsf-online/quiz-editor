2a0b74682d32837744250fbe69bb0bf12bd343db
cadenjackson11 <143419483+cadenjackson11@users.noreply.github.com>
2024-04-04 09:43:39 -0500

    got add and remove questions finished up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,25 +13,22 @@ interface QuestionDisplay {
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
   title = 'quiz-editor';
 
-  constructor(
-    public quizSvc: QuizService
-  ) {
-  }
+  constructor(public quizSvc: QuizService) {}
 
   ngOnInit() {
     const quizzes = this.quizSvc.loadQuizzes();
     console.log(quizzes);
 
-    this.quizzes = quizzes.map(x => ({
-      quizName: x.name
-      , quizQuestions: x.questions.map((y: any) => ({
-        questionName: y.name
-      }))
+    this.quizzes = quizzes.map((x) => ({
+      quizName: x.name,
+      quizQuestions: x.questions.map((y: any) => ({
+        questionName: y.name,
+      })),
     }));
 
     console.log(this.quizzes);
@@ -48,15 +45,31 @@ export class AppComponent implements OnInit {
 
   addNewQuiz = () => {
     const newQuiz = {
-      quizName: "Untitled Quiz"
-      , quizQuestions: []
+      quizName: 'Untitled Quiz',
+      quizQuestions: [],
     };
 
-    this.quizzes = [
-      ...this.quizzes
-      , newQuiz 
-    ];
+    this.quizzes = [...this.quizzes, newQuiz];
 
     this.selectedQuiz = newQuiz;
+  };
+
+  addNewQuestion = () => {
+    if (this.selectedQuiz) {
+      this.selectedQuiz.quizQuestions = [
+        ...this.selectedQuiz.quizQuestions,
+        {
+          questionName: 'Untitled Question',
+        },
+      ];
+    }
+  };
+
+  removeQuestion = (questionToRemove: QuestionDisplay) => {
+    if (this.selectedQuiz) {
+      this.selectedQuiz.quizQuestions = this.selectedQuiz.quizQuestions.filter(
+        (x) => x !== questionToRemove
+      );
+    }
   };
 }
