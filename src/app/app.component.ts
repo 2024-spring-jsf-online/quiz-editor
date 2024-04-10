0757247d0e3f532d70994537c7f108c1a39ddc4d
cadenjackson11 <143419483+cadenjackson11@users.noreply.github.com>
2024-04-09 23:12:00 -0500

    Created error message for the quiz editor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,6 +21,8 @@ export class AppComponent implements OnInit {
 
   constructor(public quizSvc: QuizService) {}
 
+  errorLoadingQuizzes = false;
+
   ngOnInit() {
     const quizzes = this.quizSvc.loadQuizzes();
     console.log(quizzes);
@@ -37,7 +39,8 @@ export class AppComponent implements OnInit {
         }));
       },
       (err) => {
-        console.error(err);
+        console.error(err.error);
+        this.errorLoadingQuizzes = true;
       }
     );
 
