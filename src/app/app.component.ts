e27a01735571371792c0b6222446a0441b57babe
cadenjackson11 <143419483+cadenjackson11@users.noreply.github.com>
2024-04-14 10:21:25 -0500

    added a cancel all changes function
    added a functionality to the cancel button that actually cancels all changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,39 +14,34 @@ interface QuestionDisplay {
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
   title = 'quiz-editor';
 
-  constructor(
-    public quizSvc: QuizService
-  ) {
-  }
+  constructor(public quizSvc: QuizService) {}
 
   loading = true;
   errorLoadingQuizzes = false;
 
   loadQuizzesFromCloud = async () => {
-
     try {
-      const quizzes = await this.quizSvc.loadQuizzes() ?? [];
+      const quizzes = (await this.quizSvc.loadQuizzes()) ?? [];
       console.log(quizzes);
 
-      this.quizzes = quizzes.map(x => ({
-        quizName: x.name
-        , quizQuestions: x.questions.map(y => ({
-          questionName: y.name
-        }))
-        , markedForDelete: false
-      }));      
+      this.quizzes = quizzes.map((x) => ({
+        quizName: x.name,
+        quizQuestions: x.questions.map((y) => ({
+          questionName: y.name,
+        })),
+        markedForDelete: false,
+      }));
 
       this.loading = false;
-    }
-    catch (err) {
+    } catch (err) {
       console.error(err);
       this.errorLoadingQuizzes = true;
-      this.loading = false;      
+      this.loading = false;
     }
   };
 
@@ -65,75 +60,64 @@ export class AppComponent implements OnInit {
 
   addNewQuiz = () => {
     const newQuiz = {
-      quizName: "Untitled Quiz"
-      , quizQuestions: []
-      , markedForDelete: false
+      quizName: 'Untitled Quiz',
+      quizQuestions: [],
+      markedForDelete: false,
     };
 
-    this.quizzes = [
-      ...this.quizzes
-      , newQuiz 
-    ];
+    this.quizzes = [...this.quizzes, newQuiz];
 
     this.selectedQuiz = newQuiz;
   };
 
   addNewQuestion = () => {
-    
     if (this.selectedQuiz) {
       this.selectedQuiz.quizQuestions = [
-        ...this.selectedQuiz.quizQuestions
-        , {
-          questionName: "Untitled Question"
-        }
+        ...this.selectedQuiz.quizQuestions,
+        {
+          questionName: 'Untitled Question',
+        },
       ];
     }
   };
 
   removeQuestion = (questionToRemove: QuestionDisplay) => {
     if (this.selectedQuiz) {
-      this.selectedQuiz.quizQuestions = this.selectedQuiz.quizQuestions.filter(x => x !== questionToRemove);
+      this.selectedQuiz.quizQuestions = this.selectedQuiz.quizQuestions.filter(
+        (x) => x !== questionToRemove
+      );
     }
   };
 
-
   jsPromisesOne = () => {
     const n = this.quizSvc.getMagicNumber(true);
-    console.log(n); // ? ? ? 
+    console.log(n); // ? ? ?
 
-    n.then(
-      number => {
-        console.log(number);
+    n.then((number) => {
+      console.log(number);
 
-        const n2 = this.quizSvc.getMagicNumber(true);
-        console.log(n2); // ? ? ?
-        
-        n2.then(x => console.log(x)).catch(e => console.error(e));
-      }
-    ).catch(
-      err => {
-        console.error(err);
-      }
-    )
+      const n2 = this.quizSvc.getMagicNumber(true);
+      console.log(n2); // ? ? ?
+
+      n2.then((x) => console.log(x)).catch((e) => console.error(e));
+    }).catch((err) => {
+      console.error(err);
+    });
   };
 
   jsPromisesTwo = async () => {
-
     try {
       const x = await this.quizSvc.getMagicNumber(true);
       console.log(x); // ? ? ?
 
       const y = await this.quizSvc.getMagicNumber(true);
       console.log(y); // ? ? ?
-    }
-    
-    catch (err) {
+    } catch (err) {
       console.error(err);
     }
   };
 
   jsPromisesThree = async () => {
-
     try {
       const x = this.quizSvc.getMagicNumber(true);
       console.log(x); // ? ? ?
@@ -144,11 +128,13 @@ export class AppComponent implements OnInit {
       const results = await Promise.all([x, y]);
       // const results = await Promise.race([x, y]);
       console.log(results); // ? ? ?
-    }
-    
-    catch (err) {
+    } catch (err) {
       console.error(err);
     }
   };
 
+  cancelAllChanges = () => {
+    this.loadQuizzesFromCloud();
+    this.selectedQuiz = undefined;
+  };
 }
