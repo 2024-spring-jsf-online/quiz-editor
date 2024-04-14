1c1f408dacdda66ef29a3fa116dfd44faf333cd3
CEBlank <96022511+CEBlank@users.noreply.github.com>
2024-04-14 02:11:55 -0500

    Async Await Promises
    code away from subscribe, added cloud call quiz list.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,29 +26,53 @@ export class AppComponent implements OnInit {
 
   errorLoadingQuizzes = false;
 
+  loadQuizzesFromCloud = async () => {
+
+    try {
+      const quizzes = await this.quizSvc.loadQuizzes() ?? [];
+      console.log(quizzes);
+    this.quizzes = quizzes.map(x => ({
+       quizName: x.name
+     , quizQuestions: x.questions.map((y: any) => ({
+        questionName: y.name
+      })),
+        markedForDelete: false
+      }));
+    }
+    catch (err) {
+      console.error(err);
+      this.errorLoadingQuizzes = true;
+    }
+  };
+
   ngOnInit() {
-    const quizzes = this.quizSvc.loadQuizzes();
-    console.log(quizzes);
+    this.loadQuizzesFromCloud();
+    console.log(this.quizzes);
+  }
+ //   const quizzes = this.quizSvc.loadQuizzes() ?? [];
+ //   console.log(quizzes);
 
-    quizzes.subscribe({
-      next: data => {
-        console.log(data);
-        this.quizzes = data.map( x => ({
-          quizName: x.name,
-          quizQuestions: x.questions.map(y => ({
-            questionName: y.name
-          })),
-          markedForDelete: false
-        }));
+ //   quizzes.subscribe({
+//      next: data => {
+ //       console.log(data);
+ //       this.quizzes = data.map( x => ({
+ //         quizName: x.name,
+  //        quizQuestions: x.questions.map(y => ({
+ //           questionName: y.name
+//          })),
+ //         markedForDelete: false
+ //       }));
 
-      },
-      error: err => {
-        console.error(err.error);
-        this.errorLoadingQuizzes = true;
-      }}
-    );
+//      },
+//      error: err => {
+ //       console.error(err.error);
+ //       this.errorLoadingQuizzes = true;
+ //     }}
+  //  );
 
-
+   // ngOnInit() {
+   //   this.
+   // }
 
   //  this.quizzes = quizzes.map(x => ({
   //    quizName: x.name
@@ -58,8 +82,7 @@ export class AppComponent implements OnInit {
   //    , markedForDelete: false
   //  }));
 
-    console.log(this.quizzes);
-  }
+
 
   quizzes: QuizDisplay[] = [];
 
@@ -102,5 +125,29 @@ export class AppComponent implements OnInit {
       this.selectedQuiz.quizQuestions = this.selectedQuiz.quizQuestions.filter(x => x !== questionToRemove);
     }
   };
+
+
+  jsPromisesOne = () => {
+    const n = this.quizSvc.getMagicNumber(true);
+    console.log(n)
+
+    n.then(
+      number => {
+        console.log(number);
+      }
+    )
+  };
+
+  jsPromisesTwo = async () => {
+    try {
+    const x = await this.quizSvc.getMagicNumber(true);
+    console.log(x);
+   }
+
+   catch (err) {
+    console.error(err);
+   }
+  };
+
 }
 
