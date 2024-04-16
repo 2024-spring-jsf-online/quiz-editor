6239c2a6c0c734d52822772ff29a7bf3824dc7e9
JA8866 <109983530+JA8866@users.noreply.github.com>
2024-04-16 11:12:38 -0500

    Got edited quiz badge working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ interface QuizDisplay {
   quizQuestions: QuestionDisplay[];
   markedForDelete: boolean;
   newlyAddedQuiz: boolean;
+  naiveQuizChecksum: string;
 }
 
 interface QuestionDisplay {
@@ -28,6 +29,12 @@ export class AppComponent implements OnInit {
   loading = true;
   errorLoadingQuizzes = false;
 
+  generateNaiveQuizChecksum = (quiz: QuizFromWeb) => {
+    return quiz.name + quiz.questions.map(
+      (x) => '~' + x.name
+    ).join('');
+  };
+
   loadQuizzesFromCloud = async () => {
 
     try {
@@ -41,6 +48,7 @@ export class AppComponent implements OnInit {
         }))
         , markedForDelete: false
         , newlyAddedQuiz: false
+        , naiveQuizChecksum: this.generateNaiveQuizChecksum(x)
       }));      
 
       this.loading = false;
@@ -71,6 +79,7 @@ export class AppComponent implements OnInit {
       , quizQuestions: []
       , markedForDelete: false
       , newlyAddedQuiz: true
+      , naiveQuizChecksum: ""
     };
 
     this.quizzes = [
@@ -173,5 +182,18 @@ export class AppComponent implements OnInit {
 
   get addedQuizCount() {
     return this.getAddedQuizzes().length;
+  };
+
+  getEditedQuizzes = () => {
+    return this.quizzes.filter((x) =>
+      x.quizName + x.quizQuestions.map(
+        (y) => '~' + y.questionName
+      ).join('') !== x.naiveQuizChecksum
+      && !x.newlyAddedQuiz && !x.markedForDelete
+    );
+  };
+
+  get editedQuizCount() {
+    return this.getEditedQuizzes().length;
   };
 }
