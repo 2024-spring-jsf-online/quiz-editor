54b8a9a642ed9d98b2ecfc957d5b7c6d4884b537
JA8866 <109983530+JA8866@users.noreply.github.com>
2024-04-25 16:02:04 -0500

    finished week 14 demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { QuizService, QuizFromWeb } from './quiz.service';
+import { 
+  QuizService,
+  QuizFromWeb,
+  ShapeForSavingNewQuizzes,
+  ShapeForSavingEditedQuizzes
+} from './quiz.service';
 
 import {
   trigger,
@@ -229,5 +234,29 @@ export class AppComponent implements OnInit {
 
   detailsFromLeftAnimationDone = () => {
     this.detailsFromLeftAnimationState = "leftPosition";
+  };
+
+  saveQuizzes = async () => {
+    try {
+      const newQuizzes: ShapeForSavingNewQuizzes[] = [];
+      
+      const editedQuizzes: ShapeForSavingEditedQuizzes[] = 
+          this.getEditedQuizzes().map((x) => ({
+              quiz: x.quizName,
+              questions: x.quizQuestions.map((y) => ({
+                question: y.questionName
+              }))
+            })
+          );
+
+      const numberOfUpdatedQuizzes = await this.quizSvc.saveQuizzes(
+        editedQuizzes,
+        newQuizzes
+      );
+      console.log(`numberOfUpdatedQuizzes: ${numberOfUpdatedQuizzes}`);
+    }
+    catch (err) {
+      console.error(err);
+    }
   };
 }
