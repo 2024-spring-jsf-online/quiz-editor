5253f0d395754d36790e975f6fa36ef0f53c87ed
PopcornProblems <126116250+PopcornProblems@users.noreply.github.com>
2024-05-09 09:49:49 -0500

    Using the non deprecated version of subscribe
    Angular just keeps on a changing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -28,8 +28,8 @@ export class AppComponent implements OnInit {
     const quizzes = this.quizSvc.loadQuizzes();
     console.log(quizzes);
 
-    quizzes.subscribe(
-      data => {
+    quizzes.subscribe({
+      next: data => {
         console.log(data);
         this.quizzes = data.map(x => ({
           quizName: x.name
@@ -39,10 +39,10 @@ export class AppComponent implements OnInit {
           , markedForDelete: false
         }));
       }
-      , err => {
+      ,error: err => {
         console.error(err);
       }
-    );
+    });
 
     //this.quizzes = quizzes.map(x => ({
     //  quizName: x.name
